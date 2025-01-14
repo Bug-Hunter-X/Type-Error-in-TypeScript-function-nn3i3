31f59d9c3eff216ef008349328e20b57c2fadce7
BugHunter-X <askzenai@gmail.com>
2025-01-14 08:56:05 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,4 @@
+function greet(person: string, date: Date): string {
+  console.log(`Hello, ${person}, today is ${date.toDateString()}.`);
+  return `Hello, ${person}, today is ${date.toDateString()}.`;
+}
